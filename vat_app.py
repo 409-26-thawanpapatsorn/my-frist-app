787d113e251f943522ee45d14b4cd2d56ecd7774
409-26-thawanpapatsorn <54575@yupparaj.ac.th>
2026-08-04 14:16:09 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -6,4 +6,4 @@ net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
-st.write("นางสาวธวัลปภัสสร ศรีปาลวิทย์")
+st.write("นางสาวธวัลปภัสสร ศรีปาลวิทย์ ม.4/9 เลขที่ 26 ")
