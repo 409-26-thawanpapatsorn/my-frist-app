256f04a3162ad152b5a364c07262e823ca0a9223
409-26-thawanpapatsorn <54575@yupparaj.ac.th>
2026-08-25 14:26:31 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -51,7 +51,7 @@ def show_result_dialog(ans1, ans2):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
     # ตรวจข้อ 3
-    if u_ans2 == "book":
+    if u_ans3 == "book":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
